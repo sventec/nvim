89b014d10f503b8df8d4235ae7bc536898ab403e
sventec <datheadcrabtho@gmail.com>
2022-02-02 01:35:30 -0500

    Add date snippet and remove dead code

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -36,6 +36,21 @@ require("luasnip.loaders.from_snipmate").load()
 -- extend the _.snippets file for global filetypes
 luasnip.filetype_extend("all", { "_" })
 
+-- insert date with luasnip
+local date = function()
+	return { os.date("%Y-%m-%d") }
+end
+
+luasnip.snippets = {
+	all = {
+		luasnip.snippet({
+			trig = "date",
+			namr = "Date",
+			dscr = "Date in the form of YYYY-MM-DD",
+		}),
+	},
+}
+
 local check_backspace = function()
 	local col = vim.fn.col(".") - 1
 	return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
@@ -197,53 +212,3 @@ cmp.setup.cmdline("/", {
 		{ name = "buffer" },
 	},
 })
-
--- cmp.setup({
--- 	snippet = {
--- 		expand = function(args)
--- 			require("luasnip").lsp_expand(args.body)
--- 		end,
--- 	},
--- 	completion = {
--- 		keyword_length = 2,
--- 	},
--- 	mapping = {
--- 		-- standard keybinds
--- 		["<C-p>"] = cmp.mapping.select_prev_item(),
--- 		["<C-n>"] = cmp.mapping.select_next_item(),
--- 		["<C-d>"] = cmp.mapping.scroll_docs(-4),
--- 		["<C-f>"] = cmp.mapping.scroll_docs(4),
--- 		["<C-Space>"] = cmp.mapping.complete(),
--- 		["<C-e>"] = cmp.mapping.close(),
--- 		["<CR>"] = cmp.mapping.confirm({
--- 			behavior = cmp.ConfirmBehavior.Replace,
--- 			select = true,
--- 		}),
--- 		-- tab bindings
--- 		["<Tab>"] = function(fallback)
--- 			if cmp.visible() then
--- 				cmp.select_next_item()
--- 			elseif luasnip.expand_or_jumpable() then
--- 				luasnip.expand_or_jump()
--- 			else
--- 				fallback()
--- 			end
--- 		end,
--- 		["S-Tab"] = function(fallback)
--- 			if cmp.visible() then
--- 				cmp.select_prev_item()
--- 			elseif luasnip.jumpable(-1) then
--- 				luasnip.jump(-1)
--- 			else
--- 				fallback()
--- 			end
--- 		end,
--- 	},
--- 	sources = {
--- 		{ name = "nvim_lsp" },
--- 		{ name = "luasnip" },
--- 		{ name = "path" },
--- 		{ name = "buffer" },
--- 	},
---   ghost_text = true,
--- })
