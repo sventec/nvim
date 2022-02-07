7766b106511a478340ce00a7854fd98e69102eb8
Reed Simon <reed.simon@uvm.edu>
2022-02-06 19:06:38 -0500

    update indent-blanline config

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -5,44 +5,52 @@ if not status_ok then
 	return
 end
 
-vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
-vim.g.indent_blankline_filetype_exclude = {
-	"help",
-	"startify",
-	"dashboard",
-	"packer",
-	"neogitstatus",
-	"NvimTree",
-	"Trouble",
-}
-vim.g.indent_blankline_show_first_indent_level = true
-vim.g.indent_blankline_use_treesitter = true
-vim.g.indent_blankline_show_current_context = true
-vim.g.indent_blankline_context_patterns = {
-	"class",
-	"return",
-	"function",
-	"method",
-	"^if",
-	"^while",
-	"jsx_element",
-	"^for",
-	"^object",
-	"^table",
-	"block",
-	"arguments",
-	"if_statement",
-	"else_clause",
-	"jsx_element",
-	"jsx_self_closing_element",
-	"try_statement",
-	"catch_clause",
-	"import_statement",
-	"operation_type",
-}
-vim.g.indent_blankline_char = "▏"
-
 indent_blankline.setup({
+	indentLine_enabled = 1,
+	char = "▏",
+	filetype_exclude = {
+		"help",
+		"terminal",
+		"dashboard",
+		"packer",
+		"lspinfo",
+		"TelescopePrompt",
+		"TelescopeResults",
+		"nvchad_cheatsheet",
+		"lsp-installer",
+		"startify",
+		"neogitstatus",
+		"NvimTree",
+		"Trouble",
+		"",
+	},
+	buftype_exclude = { "terminal", "nofile" },
+	show_trailing_blankline_indent = false,
+	show_first_indent_level = false,
 	show_current_context = true,
 	show_current_context_start = false, -- seems to be buggy atm
+	-- use_treesitter = true,
 })
+
+-- vim.g.indent_blankline_context_patterns = {
+-- 	"class",
+-- 	"return",
+-- 	"function",
+-- 	"method",
+-- 	"^if",
+-- 	"^while",
+-- 	"jsx_element",
+-- 	"^for",
+-- 	"^object",
+-- 	"^table",
+-- 	"block",
+-- 	"arguments",
+-- 	"if_statement",
+-- 	"else_clause",
+-- 	"jsx_element",
+-- 	"jsx_self_closing_element",
+-- 	"try_statement",
+-- 	"catch_clause",
+-- 	"import_statement",
+-- 	"operation_type",
+-- }
