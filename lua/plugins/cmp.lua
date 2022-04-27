6ca1a2285d79a5e2182a9f1ad53bec6278c1f722
sventec <fx@fewrx.net>
2022-04-27 16:00:36 -0400

    Update nvim-cmp documentation for nvim 0.7

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -154,9 +154,11 @@ cmp.setup({
 		behavior = cmp.ConfirmBehavior.Replace,
 		select = false,
 	},
-	documentation = {
-		border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-	},
+  window = {
+    documentation = {
+      border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+    },
+  },
 	experimental = {
 		ghost_text = false,
 		native_menu = false,
