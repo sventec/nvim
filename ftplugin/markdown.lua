f1c8b1a81d7787874eeb47ad94a1fa258016d3a5
sventec <fx@fewrx.net>
2022-06-19 03:13:02 -0400

    Add soft wrapping to txt and markdown

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,2 +1,10 @@
+-- markdown language config
+
 -- set spelling for buffer only
 vim.opt_local.spell = true
+
+-- line width for wrapping
+vim.opt.textwidth = 120
+
+-- soft wrapping
+vim.opt.wrap = true
--- ftplugin/txt.lua
+++ ftplugin/txt.lua
@@ -5,3 +5,9 @@
 
 -- set spelling for buffer only
 vim.opt_local.spell = true
+
+-- line width for wrapping
+vim.opt.textwidth = 120
+
+-- soft wrapping
+vim.opt.wrap = true
