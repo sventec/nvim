cfe09aa31a5ef8d8d2467a0bcc2969eab7730439
sventec <fx@fewrx.net>
2023-05-21 01:23:47 -0400

    Move autocommands, fix 'gq' behavior with active LSP

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,18 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Don't auto-comment new lines after commented line
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+  pattern = "*",
+  command = "setlocal formatoptions-=ro",
+})
+
+-- Use internal formatting for bindings like gq
+-- See: https://github.com/jose-elias-alvarez/null-ls.nvim/issues/1131#issuecomment-1268760653
+-- W/o this, null-ls source attached to buffer can cause 'gq' to not work
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    vim.bo[args.buf].formatexpr = nil
+  end,
+})
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -21,7 +21,3 @@ opt.colorcolumn = { "81", "89", "121" } -- vertical marker at columns 81, 89, 12
 
 -- ==misc. QOL== --
 cmd([[set cindent cinkeys-=0#]]) -- indenting with > and < also indents comments
-
--- don't auto comment new lines (e.g. via o/O)
--- credit brainfucksec/neovim-lua
-cmd([[au BufEnter * set fo-=c fo-=r fo-=o]])
